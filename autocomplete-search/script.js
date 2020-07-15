26b6afee291b6780d09657b28cd3269dff44e557
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-07-15 18:04:13 +0200

    auto search: mocking the server response

---
+++ autocomplete-search/script.js
@@ -0,0 +1,87 @@
+const BASE = "server.com";
+const searchInput = document.getElementsByClassName("search__bar__input")[0];
+
+const onSuggestionesponse = data => {
+  const suggestionElement = document.getElementsByClassName(
+    "search__suggestions__list"
+  );
+  suggestionElement.innerHTML += data + "<br>";
+};
+
+const onNewInput = e => {
+  api.get(`${BASE}/autocomplete`, searchInput.value, onSuggestionesponse);
+};
+searchInput.addEventListener("input", onNewInput);
+
+// Mock the server
+const getRandomString = ({ length }) => {
+  const charSet =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ";
+  const chars = [];
+  while (chars.length < length) {
+    const randomIdx = Math.floor(Math.random() * charSet.length);
+    chars.push(charSet[randomIdx]);
+  }
+  return chars.join("");
+};
+const getRandomInteger = ({ min, max }) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+const getSuggestion = prefix => {
+  const EXACT_MATCH_RATIO = 0.3;
+  const AUTOCORRECT_RATIO = 0.1;
+
+  if (Math.random() < AUTOCORRECT_RATIO)
+    return getRandomString({
+      length: getRandomInteger({ min: 1, max: prefix.length })
+    });
+
+  if (Math.random() < EXACT_MATCH_RATIO) return prefix;
+
+  return (
+    prefix + getRandomString({ length: getRandomInteger({ min: 1, max: 10 }) })
+  );
+};
+const getSuggestionsList = data => {
+  const MAX_CHARS = 10;
+  const NUM_AUTOCOMPLETE_RESULTS = 10;
+  const AUXILIART_DATA_RATIO = 0.1;
+
+  if (data.length > MAX_CHARS) return [];
+
+  const results = [];
+  while (results.length < NUM_AUTOCOMPLETE_RESULTS) {
+    const suggestion = getSuggestion(data);
+    if (results.find(result => result.suggestion === suggestion)) continue;
+
+    if (Math.random() < AUXILIART_DATA_RATIO) {
+      for (let i = 0; i < 2; i++) {
+        results.push({
+          suggestion,
+          auxiliary: getRandomString({
+            length: getRandomInteger({ min: 5, max: 15 })
+          })
+        });
+      }
+    } else results.push({ suggestion, auxiliary: "" });
+  }
+  return results;
+};
+const endpoints = {
+  "/": {
+    get: () => "Hello, World!"
+  },
+  "/autocomplete": {
+    get: getSuggestionsList
+  }
+};
+
+// Mock The API
+const handleGet = (url, data, callback) => {
+  const base = url.substring(0, url.indexOf("/"));
+  const endpoint = url.substring(url.indexOf("/"), url.length);
+  callback(endpoints[endpoint]["get"](data));
+};
+const api = {
+  get: handleGet
+};
